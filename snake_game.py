b81975f4049033a40a05adad41337bac5c8993d1
jagdeep sidhu <sidhujag@syscoin.org>
2024-01-24 18:15:02 -0800

    aider: Create the file `snake_game.py` with the initial Python code for the CLI snake game.
    # Aider chat conversation:
    
    USER: Please create the file `snake_game.py` with the following Python code to initiate the base of the CLI snake game:
    
    ```python
    import random
    import curses
    
    def create_food(snake, box):
        food = None
        while food is None:
            food = [random.randint(box[0][0]+1, box[1][0]-1), random.randint(box[0][1]+1, box[1][1]-1)]
            if food in snake:
                food is None
        return food
    
    def main(stdscr):
        curses.curs_set(0)
        stdscr.nodelay(1)
        stdscr.timeout(100)
    
        sh, sw = stdscr.getmaxyx()
        box = [[3,3], [sh-3, sw-3]]
        stdscr.box()
    
        snake = [[sh//2, sw//2 + 1], [sh//2, sw//2], [sh//2, sw//2 - 1]]
        direction = curses.KEY_RIGHT
    
        for y, x in snake:
            stdscr.addch(y, x, '#')
    
        food = create_food(snake, box)
        stdscr.addch(food[0], food[1], '*')
    
        while True:
            next_key = stdscr.getch()
            direction = direction if next_key == -1 else next_key
    
            head = [snake[0][0], snake[0][1]]
    
            if direction == curses.KEY_DOWN:
                head[0] += 1
            if direction == curses.KEY_UP:
                head[0] -= 1
            if direction == curses.KEY_LEFT:
                head[1] -= 1
            if direction == curses.KEY_RIGHT:
                head[1] += 1
    
            snake.insert(0, head)
    
            if snake[0] == food:
                food = create_food(snake, box)
                stdscr.addch(food[0], food[1], '*')
            else:
                tail = snake.pop()
                stdscr.addch(tail[0], tail[1], ' ')
    
            if (snake[0][0] in [box[0][0], box[1][0]] or
                snake[0][1] in [box[0][1], box[1][1]] or
                snake[0] in snake[1:]):
                break
    
            stdscr.addch(snake[0][0], snake[0][1], '#')
    
    curses.wrapper(main)
    ```
    
    ASSISTANT: Here is the diff for creating the new file `snake_game.py` with the provided Python code:
    
    ```diff
    --- /dev/null
    +++ snake_game.py
    @@ ... @@
    +import random
    +import curses
    +
    +def create_food(snake, box):
    +    food = None
    +    while food is None:
    +        food = [random.randint(box[0][0]+1, box[1][0]-1), random.randint(box[0][1]+1, box[1][1]-1)]
    +        if food in snake:
    +            food is None
    +    return food
    +
    +def main(stdscr):
    +    curses.curs_set(0)
    +    stdscr.nodelay(1)
    +    stdscr.timeout(100)
    +
    +    sh, sw = stdscr.getmaxyx()
    +    box = [[3,3], [sh-3, sw-3]]
    +    stdscr.box()
    +
    +    snake = [[sh//2, sw//2 + 1], [sh//2, sw//2], [sh//2, sw//2 - 1]]
    +    direction = curses.KEY_RIGHT
    +
    +    for y, x in snake:
    +        stdscr.addch(y, x, '#')
    +
    +    food = create_food(snake, box)
    +    stdscr.addch(food[0], food[1], '*')
    +
    +    while True:
    +        next_key = stdscr.getch()
    +        direction = direction if next_key == -1 else next_key
    +
    +        head = [snake[0][0], snake[0][1]]
    +
    +        if direction == curses.KEY_DOWN:
    +            head[0] += 1
    +        if direction == curses.KEY_UP:
    +            head[0] -= 1
    +        if direction == curses.KEY_LEFT:
    +            head[1] -= 1
    +        if direction == curses.KEY_RIGHT:
    +            head[1] += 1
    +
    +        snake.insert(0, head)
    +
    +        if snake[0] == food:
    +            food = create_food(snake, box)
    +            stdscr.addch(food[0], food[1], '*')
    +        else:
    +            tail = snake.pop()
    +            stdscr.addch(tail[0], tail[1], ' ')
    +
    +        if (snake[0][0] in [box[0][0], box[1][0]] or
    +            snake[0][1] in [box[0][1], box[1][1]] or
    +            snake[0] in snake[1:]):
    +            break
    +
    +        stdscr.addch(snake[0][0], snake[0][1], '#')
    +
    +curses.wrapper(main)
    ```
    
    This diff represents the creation of a new file with the initial code for the CLI snake game.

--- snake_game.py
+++ snake_game.py
@@ -406,3 +406,64 @@ def main(stdscr):
         stdscr.addch(snake[0][0], snake[0][1], '#')
 
 curses.wrapper(main)
+import random
+import curses
+
+def create_food(snake, box):
+    food = None
+    while food is None:
+        food = [random.randint(box[0][0]+1, box[1][0]-1), random.randint(box[0][1]+1, box[1][1]-1)]
+        if food in snake:
+            food is None
+    return food
+
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.nodelay(1)
+    stdscr.timeout(100)
+
+    sh, sw = stdscr.getmaxyx()
+    box = [[3,3], [sh-3, sw-3]]
+    stdscr.box()
+
+    snake = [[sh//2, sw//2 + 1], [sh//2, sw//2], [sh//2, sw//2 - 1]]
+    direction = curses.KEY_RIGHT
+
+    for y, x in snake:
+        stdscr.addch(y, x, '#')
+
+    food = create_food(snake, box)
+    stdscr.addch(food[0], food[1], '*')
+
+    while True:
+        next_key = stdscr.getch()
+        direction = direction if next_key == -1 else next_key
+
+        head = [snake[0][0], snake[0][1]]
+
+        if direction == curses.KEY_DOWN:
+            head[0] += 1
+        if direction == curses.KEY_UP:
+            head[0] -= 1
+        if direction == curses.KEY_LEFT:
+            head[1] -= 1
+        if direction == curses.KEY_RIGHT:
+            head[1] += 1
+
+        snake.insert(0, head)
+
+        if snake[0] == food:
+            food = create_food(snake, box)
+            stdscr.addch(food[0], food[1], '*')
+        else:
+            tail = snake.pop()
+            stdscr.addch(tail[0], tail[1], ' ')
+
+        if (snake[0][0] in [box[0][0], box[1][0]] or
+            snake[0][1] in [box[0][1], box[1][1]] or
+            snake[0] in snake[1:]):
+            break
+
+        stdscr.addch(snake[0][0], snake[0][1], '#')
+
+curses.wrapper(main)
